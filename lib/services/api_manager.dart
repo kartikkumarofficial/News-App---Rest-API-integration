a5b17f684a06179f1b261d71b8b8d49c1ca0ed21
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-03 19:33:46 +0530

    homepage

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,9 +1,24 @@
-import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class API_Manager{
-  void getNews(){
+import 'package:http/http.dart' as http;
+import 'package:newsapp/models/newsInfo.dart';
+
+class API_Manager {
+  void getNews() async {
     var client = http.Client();
-    client.get(Uri.parse('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=c423cad5df3948baa6dc529c5693b443'));
+    var response = await client.get(Uri.parse('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=c423cad5df3948baa6dc529c5693b443'));
+    if(response.statusCode==200){
+      var jsonString = response.body;
+      var jsonMap = json.decode(jsonString);
+      // var jsonMap = newsModelFromJson(jsonString);
+      NewsModel.fromJson(jsonMap);
+
+
+
+
+
+    }
+
 
 
   }
