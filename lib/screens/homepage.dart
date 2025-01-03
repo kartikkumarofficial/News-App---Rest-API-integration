63ca304f90000481ddc55bebc5cdffc7655a65de
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-03 22:40:42 +0530

    homepage

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/models/newsInfo.dart';
 import 'package:newsapp/services/api_manager.dart';
-
+import 'package:intl/intl.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -18,7 +18,6 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,29 +25,61 @@ class _HomePageState extends State<HomePage> {
         title: Text('News App'),
       ),
       body: Container(
-        child: FutureBuilder<NewsModel>(
+        child: FutureBuilder<NewsModel?>(
           future: _newsModel,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               return ListView.builder(
-                  itemCount: snapshot.data?.articles.length ,
-                  itemBuilder: (context, index) {
-                    var article = snapshot.data?.articles[index];
-                return Container(
-                  height: 100,
-                  width: double.infinity,
-                  child:
-                  Row(
-                    children: <Widget>[
-                      Image.network(article!.urlToImage),
-
-                    ],
-                  ),
-                );
-              });
-            }
-
-            else {
+                itemCount: snapshot.data?.articles.length,
+                itemBuilder: (context, index) {
+                  var article = snapshot.data?.articles[index];
+                  // var formattedTime = DateFormat('dd MMM - HH:mm');
+                  return Container(
+                    height: 100,
+                    width: double.infinity,
+                    child: Row(
+                      children: <Widget>[
+                        Card(
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Image.network(
+                              article!.urlToImage,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 16),
+                        Flexible(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              // Text(formattedTime),
+                              Text(
+                                article.title,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                article.description,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            } else {
               return Center(child: CircularProgressIndicator());
             }
           },
