a81800ba071c9f9a02f6fe454bfa38024ef72198
kartikkumarofficial <work.kartikkumar@gmail.com>
2025-01-03 20:31:33 +0530

    homepage

--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,25 +1,30 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:newsapp/models/newsInfo.dart';
 
 class API_Manager {
-  void getNews() async {
+  Future<NewsModel?> getNews() async {
     var client = http.Client();
-    var response = await client.get(Uri.parse('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=c423cad5df3948baa6dc529c5693b443'));
-    if(response.statusCode==200){
-      var jsonString = response.body;
-      var jsonMap = json.decode(jsonString);
-      // var jsonMap = newsModelFromJson(jsonString);
-      NewsModel.fromJson(jsonMap);
+    NewsModel? newsModel;
 
+    try {
+      var response = await client.get(Uri.parse(
+        'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=c423cad5df3948baa6dc529c5693b443',
+      ));
 
-
-
-
+      if (response.statusCode == 200) {
+        var jsonString = response.body; // Raw JSON response
+        var jsonMap = json.decode(jsonString); // Decode JSON into a Map
+        newsModel = NewsModel.fromJson(jsonMap); // Map JSON to NewsModel object
+      } else {
+        print('Failed to load news. Status Code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    } finally {
+      client.close(); // Clean up the HTTP client
     }
 
-
-
+    return newsModel; // Return the newsModel (null if there was an error)
   }
 }
